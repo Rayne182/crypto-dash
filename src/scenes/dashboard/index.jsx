8d38b02aa374528aba022899e73394a96a985f91
Rayne182 <rynhardtpotgieter@hotmail.com>
2023-06-04 20:14:57 +0200

    Uncommented dashboard header for now

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -8,10 +8,10 @@ const Dashboard = () => {
     return (
       <Box m="20px">
         {/* HEADER */}
-            <Box display="flex" justifyContent="space-between" alignItems="center">
+            {/*<Box display="flex" justifyContent="space-between" alignItems="center">
             <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
-            </Box>
+    </Box>*/}
         </Box>
     );
   };
