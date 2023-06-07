f5eacaffb9ff66947531b3e362121b31ff7765ee
Rayne182 <rynhardtpotgieter@hotmail.com>
2023-06-07 23:02:12 +0200

    Added metamask wallet connection (still has bugs)

--- src/components/WalletBalanceTable.jsx
+++ src/components/WalletBalanceTable.jsx
@@ -2,48 +2,73 @@ import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Box, useTheme } from "@mui/material";
 import { tokens } from '../theme';
+import Web3 from 'web3';
+import axios from 'axios';
+import ERC20_ABI from '../erc20abi.json';
 
 
-const WalletBalanceTable = ({ account }) => { // Update the prop name from `balances` to `account`
+const WalletBalanceTable = ({ account }) => {
   const [data, setData] = useState([]);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   useEffect(() => {
-    // Replace the following mock data with your logic to fetch wallet balances for the specified account
-    const balances = [
-      { currency: 'ETH', balance: '10' },
-      { currency: 'BTC', balance: '0.5' },
-      { currency: 'LTC', balance: '5' },
-    ];
+    const fetchData = async () => {
+      // Get the list of tokens from etherscan.io
+      const tokenResponse = await axios.get(`https://api.etherscan.io/api?module=account&action=tokentx&address=${account}&startblock=0&endblock=99999999&sort=asc&apikey=YOUR_ETHERSCAN_API_KEY`);
 
-    const newData = balances.map((item, index) => ({
-      id: index + 1,
-      currency: item.currency,
-      balance: item.balance,
-    }));
-    setData(newData);
+      const tokens = tokenResponse.data.result;
+        // Filter out duplicate tokens and only keep the last transaction of each token
+        //.reduce((tokens, tx) => {
+        //  tokens[tx.contractAddress] = tx;
+         // return tokens;
+        //}, {});
+
+        // Get the balance of each token and its price
+        const web3 = new Web3(window.ethereum);
+        const tokenData = await Promise.all(Object.values(tokens).map(async (token) => {
+        const contract = new web3.eth.Contract(ERC20_ABI, token.contractAddress);
+        const balance = await contract.methods.balanceOf(account).call();
+        // Get the price from CoinGecko
+        const priceResponse = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${token.tokenSymbol.toLowerCase()}&vs_currencies=usd`);
+        const price = priceResponse.data[token.tokenSymbol.toLowerCase()].usd;
+
+        return {
+          id: token.tokenSymbol,
+          currency: token.tokenSymbol,
+          balance: balance / (10 ** token.tokenDecimal),
+          price: price,
+          value: price * (balance / (10 ** token.tokenDecimal)),
+        };
+      }));
+
+      setData(tokenData);
+    };
+
+    fetchData();
   }, [account]);
 
   const columns = [
     { field: 'id', headerName: 'ID', flex: 1 },
     { field: 'currency', headerName: 'Currency', flex: 1 },
     { field: 'balance', headerName: 'Balance', flex: 1 },
-  ];
+    { field: 'price', headerName: 'Price (USD)', flex: 1 },
+    { field: 'value', headerName: 'Value (USD)', flex: 1 },
+];
 
-  return (
-    <Box 
-      sx={{ 
-        height: 'auto', 
-        width: '60%', 
-        margin: 'auto', 
-        '.MuiDataGrid-root .MuiDataGrid-footer': { display: 'none' },
-        '.MuiDataGrid-colCell': { fontWeight: 'bold' }
-      }}
-    >
-      <DataGrid sx={{background: `${colors.primary[400]}`}} rows={data} columns={columns} hideFooter/>
-    </Box>
-  );
+return (
+<Box 
+  sx={{ 
+    height: 'auto', 
+    width: '60%', 
+    margin: 'auto', 
+    '.MuiDataGrid-root .MuiDataGrid-footer': { display: 'none' },
+    '.MuiDataGrid-colCell': { fontWeight: 'bold' }
+  }}
+>
+  <DataGrid sx={{background: `${colors.primary[400]}`}} rows={data} columns={columns} hideFooter/>
+</Box>
+);
 };
 
 export default WalletBalanceTable;
--- src/scenes/wallet/index.jsx
+++ src/scenes/wallet/index.jsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import { Button, Box, Typography, useTheme } from "@mui/material";
 import WalletBalanceTable from '../../components/WalletBalanceTable'; 
 import Header from '../../components/Header';
 import { tokens } from '../../theme';
+import axios from 'axios';
 
 const Wallet = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const [account, setAccount] = useState(null);
     const [balance, setBalance] = useState(null);
+    const [tokenData, setTokenData] = useState([]);
 
     const loadWeb3 = async () => {
         if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        await window.ethereum.enable(); // prompts user to connect their MetaMask
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable(); // prompts user to connect their MetaMask
         } else {
-        window.alert('Non-Ethereum browser detected. Consider trying MetaMask!');
+            window.alert('Non-Ethereum browser detected. Consider trying MetaMask!');
         }
     };
 
     const loadBlockchainData = async () => {
-        const web3 = window.web3;
-        // Load account
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
-        const balance = await web3.eth.getBalance(accounts[0]);
-        setBalance(web3.utils.fromWei(balance));
+            const web3 = window.web3;
+            // Load account
+            const accounts = await web3.eth.getAccounts();
+            setAccount(accounts[0]);
+            const balance = await web3.eth.getBalance(accounts[0]);
+            setBalance(web3.utils.fromWei(balance));
+
+            // Fetch the list of tokens and their balances
+            const response = await axios.get(`https://api.etherscan.io/api?module=account&action=tokentx&address=${accounts[0]}&startblock=0&endblock=999999999&sort=asc&apikey=YOUR_ETHERSCAN_API_KEY`);
+            const tokenAddresses = [...new Set(response.data.result.map(tx => tx.contractAddress))];
+            const tokenData = await Promise.all(tokenAddresses.map(async (address) => {
+            const tokenContract = new web3.eth.Contract(ERC20_ABI, address);
+            const symbol = await tokenContract.methods.symbol().call();
+            const decimals = await tokenContract.methods.decimals().call();
+            const balance = await tokenContract.methods.balanceOf(accounts[0]).call();
+            return {
+                symbol,
+                balance: balance / (10 ** decimals),
+                contractAddress: address
+            };
+        }));
+        setTokenData(tokenData);
     };
 
     const connectWallet = async () => {
@@ -34,12 +52,22 @@ const Wallet = () => {
         await loadBlockchainData();
     };
 
+    useEffect(() => {
+        if (window.ethereum) {
+            window.ethereum.on('accountsChanged', () => {
+                loadBlockchainData();
+            });
+            window.ethereum.on('chainChanged', () => {
+                loadBlockchainData();
+            });
+        }
+    }, []);
+
     return (
         <Box ml="20px">
             {/* HEADER */}
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="WALLET" subtitle="Your balances" />
-
             </Box>
             <Box 
             sx={{ 
@@ -51,19 +79,18 @@ const Wallet = () => {
             }}
             >
                 {!account ? (
-                    <Button variant="contained" backgroundColor='black' onClick={connectWallet}>Connect Wallet</Button>
+                    <Button variant="contained" onClick={connectWallet}>Connect Wallet</Button>
                 ) : (
                     <>
-
                     <Typography variant="h4" fontWeight={'bold'}>Your Balances:</Typography>
                     <Typography color={colors.greenAccent[400]}>{account}</Typography>
                     <h2>{balance} ETH</h2>
-                    <WalletBalanceTable  account={account} />
+                    <WalletBalanceTable  account={account} tokenData={tokenData} />
                     </>
                 )}
             </Box>
         </Box>
-  );
+    );
 };
 
 export default Wallet;
