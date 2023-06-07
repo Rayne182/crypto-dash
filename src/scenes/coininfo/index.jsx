a37772761c5bf44c8bd5cc959e3e9808b1876bd6
Rayne182 <rynhardtpotgieter@hotmail.com>
2023-06-07 23:10:00 +0200

    Removied typograhpy element from coininfo

--- src/scenes/coininfo/index.jsx
+++ src/scenes/coininfo/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import CoinChart from "../../components/CoinChart";
 import { useLocation } from 'react-router-dom';
